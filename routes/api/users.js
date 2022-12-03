b07cf1475a521e5536d7e8327ff253f8994216ca
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-12-03 16:18:49 +0200

    add login ctrl

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,8 @@ const {usersCtrl} = require('../../controllers')
 
 const userRouter = express.Router()
 
-userRouter.post('/signup', validateBody(userValidation.registerUser), controllerWrapper(usersCtrl.register) )
+userRouter.post('/signup', validateBody(userValidation.registerUser), controllerWrapper(usersCtrl.register))
+
+userRouter.post('/login', validateBody(userValidation.loginUser), controllerWrapper(usersCtrl.login))
 
 module.exports = userRouter
