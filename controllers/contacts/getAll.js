0917311c3f567b98caa71161f0be160432052a02
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-12-05 00:57:19 +0200

    add pagination and favorite filter for getAll contacts

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,7 +3,9 @@ const Contact = require('../../models/contact')
 
 const getAll = async (req, res,) => {
     const owner = req.user.id
-    const result = await Contact.find({owner}).populate("owner", "email");
+    const { page = 1, limit = 10, ...query } = req.query
+    const skip = (page- 1) * limit
+    const result = await Contact.find({owner, ...query }).skip(skip).limit(limit).populate("owner", "email");
     res.status(200).json(result)
 }
 
